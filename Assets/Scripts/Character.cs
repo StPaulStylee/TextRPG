97574b5bd5dbdd6f0aef00a61344fb6ef7263fcf
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2019-04-16 14:35:48 -0500

    Create properties on Character class

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public int Energy { get; set; }
+    public int Attack { get; set; }
+    public int Defense { get; set; }
+    public int Gold { get; set; }
+    // Used to define where our character is in the dungeon
+    // Ex: (4, 7)
+    public Vector2 RoomIndex { get; set; }
+    public List<string> Inventory { get; set; }
+}
