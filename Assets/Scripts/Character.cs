91fa4e8790515fd45c4b1446b63b1c4e1470cdf4
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2019-04-19 06:54:18 -0500

    Begin implementing Enenmy class which inherits from Character

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -2,14 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character : MonoBehaviour
+namespace TextRPG
 {
-    public int Energy { get; set; }
-    public int Attack { get; set; }
-    public int Defense { get; set; }
-    public int Gold { get; set; }
-    // Used to define where our character is in the dungeon
-    // Ex: (4, 7)
-    public Vector2 RoomIndex { get; set; }
-    public List<string> Inventory { get; set; }
+    public class Character : MonoBehaviour
+    {
+        public int Energy { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int Gold { get; set; }
+        // Used to define where our character is in the dungeon
+        // Ex: (4, 7)
+        public Vector2 RoomIndex { get; set; }
+        public List<string> Inventory { get; set; }
+
+        // virtual allows and inherited class to override this method
+        public virtual  void TakeDamage(int amount)
+        {
+            Energy -= amount;
+            if (Energy <= 0)
+            {
+                Die();
+            }
+        }
+
+        public virtual void Die()
+        {
+            Debug.Log("The Character is Dead!")
+        }
+    }
 }
+
