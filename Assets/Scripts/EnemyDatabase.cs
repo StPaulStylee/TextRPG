a1e404e18e791b25da406e3acf6c3a217c0b8c9c
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2019-04-19 16:49:15 -0500

    Implement an Enemy Database using another singleton; learn about the AddRange method available on Collections

---
+++ Assets/Scripts/EnemyDatabase.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TextRPG
+{
+    public class EnemyDatabase : MonoBehaviour
+    {
+        public List<Enemy> Enemies { get; set; } = new List<Enemy>();
+        public static EnemyDatabase Instance { get; set; }
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                Instance = this;
+            }
+
+            // This is the same as doing a foreach loop over a specified collection
+            // One disadvantage of doing this is you can't really log details during development
+            Enemies.AddRange(GetComponents<Enemy>());
+            foreach(Enemy enemy in GetComponents<Enemy>())
+            {
+                Debug.Log("Found enemy of type " + enemy.GetType());
+            }
+        }
+    }
+}
+
